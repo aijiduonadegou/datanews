3db86ddb88d8cbba0d3bdf0c63f1adc23afcb1a8
hengfu.yan <hengfu.yan@mathartsys.com>
2025-06-19 01:38:00 +0800

    feat: workflow

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
     output: 'export',
+    basePath: '/out', // 所有资源路径自动添加 /out 前缀
+    assetPrefix: '/out/', // 静态资源（JS/CSS/图片）路径适配
+
 };
 
 export default nextConfig;
