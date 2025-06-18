e90470a4421ba82c005ec83757e1e073b856993b
hengfu.yan <hengfu.yan@mathartsys.com>
2025-06-19 02:08:24 +0800

    feat: update

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
     output: 'export',
-    basePath: '/datanews', // 所有资源路径自动添加 /out 前缀
-    assetPrefix: '/datanews/', // 静态资源（JS/CSS/图片）路径适配
     images: {
-        unoptimized: false,
-        path: '/datanews/',
+        unoptimized: true,
     },
 
 };
